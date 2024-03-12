de79b1e3b8478587f68861291822b7f4c00a5eaa
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-12 13:01:36 +0900

    lv1 작성중

--- main.kt
+++ main.kt
@@ -3,54 +3,8 @@ package com.example.myAssignment2
 import java.lang.Exception
 
 fun main() {
-    displayInfo()
-    while (true) {
-        print("먹고 싶은 메뉴를 선택해주세요(가게를 나가고 싶으면 -1을 입력하세요) : ")
-        try {
-            val option = readLine()!!.toInt()
-            val topokki = Topokki()
-            val fishcake = Fishcake()
-            val intestine = Intestine()
-            val fried = Fried()
-            when (option) {
-                1 -> {
-                    topokki.display()
-                }
-
-                2 -> {
-                    fishcake.display()
-                }
-
-                3 -> {
-                    intestine.display()
-                }
-
-                4 -> {
-                    fried.display()
-                }
-
-                -1 -> {
-                    println("가게를 떠납니다...")
-                    break
-                }
-
-                else -> {
-                    println("잘못된 번호를 입력하셨습니다!")
-                }
-            }
-        } catch (e: Exception) {
-            println("숫자를 입력해주세요!")
-        }
-    }
-//    when(Food) {
-//        is Topokki -> {}
-//        is Fishcake -> {}
-//        is Intestine -> {}
-//        is Fried -> {}
-//    }
+    
 }
-
-
 fun displayInfo() {
     println("========== 안오면 쫓아간다 분식집 메뉴판 ==========")
     println("=          1. 죽여주게 매운 떡볶이              =")
